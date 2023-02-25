3d07759df9fb41e43c3fcd958762b955cb802d41
eladnahum5 <eladnahum5@gmail.com>
2023-02-25 22:46:12 +0200

    added first exercise and updated .gitignore

---
+++ exercise_1/exercise_1.m
@@ -0,0 +1,27 @@
+%% Importing the data
+clear all; clc
+
+nii = xff('exercise_1\sub-025-task-intact2.nii');
+voi = xff('exercise_1\a1_group2_new.voi');
+data = nii.VoxelData;
+
+%% Get VOIs timecourse
+nii_voitc = nii.VOITimeCourse(voi, struct('weight', 3));
+nii_voitc_table = cell2table(nii_voitc);
+
+%% Get timecourse average
+[B, voin] = nii.VOITimeCourse(voi);
+B_table = array2table(B, "VariableNames", voin);
+figure(1);
+stackedplot(B_table)
+
+%% Calculatin correlation
+figure(2);
+B_corr = corrcoef(B);
+imagesc(B_corr);
+xticks(1:5)
+xticklabels(voin);
+yticks(1:5)
+yticklabels(voin);
+title('Correlation Matrix', "FontSize", 14);
+colorbar;
